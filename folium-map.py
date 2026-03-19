981b27803783583bf8e0c81c7028713c3219f141
K C Suresh Naidu <sur3sh.naidu@gmail.com>
2026-03-19 20:25:41 +0530

    Add folium map generation and HTML output for travel map

---
+++ folium-map.py
@@ -0,0 +1,27 @@
+import folium #pip install folium 
+#this is the library we need to create maps, this package helps us to create interactive maps with python
+
+
+my_map = folium.Map(location=[12.909561807850984, 77.55010552419766], zoom_start=15) 
+
+
+folium.Marker(
+    [12.909561807850984, 77.55010552419766], 
+    popup="<b>school where suresh studied</b><br>best school in the world ",
+    tooltip="Click for a tip!",
+    icon=folium.Icon(color='red', icon='info-sign')
+).add_to(my_map)
+
+
+folium.Circle(
+    radius=200, # in meters
+    location=[12.909561807850984, 77.55010552419766],
+    color='crimson',
+    fill=True,
+    fill_color='crimson'
+).add_to(my_map)
+
+
+my_map.save("my_travel_map.html") #a html file is created in the current directory you can open in in browser
+
+print(" Map created! Open 'my_travel_map.html' in your browser to see it.")
