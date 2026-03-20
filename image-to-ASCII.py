4faaccfbfd186216d6218ccf2303718842249bf0
K C Suresh Naidu <sur3sh.naidu@gmail.com>
2026-03-20 20:28:18 +0530

    Refactor code structure to improve readability and maintainability

---
+++ image-to-ASCII.py
@@ -0,0 +1,53 @@
+from PIL import Image
+
+
+ASCII_CHARS = "@%#*+=-:. "
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio * 0.55)
+    return image.resize((new_width, new_height))
+
+def grayscale(image):
+    return image.convert("L")
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = ""
+
+    for pixel in pixels:
+        
+        index = pixel * len(ASCII_CHARS) // 256
+        ascii_str += ASCII_CHARS[index]
+
+    return ascii_str
+
+
+path = input("Enter image path: ")
+
+try:
+    image = Image.open(path)
+except:
+    print(" Invalid path or file not found")
+    exit()
+
+image = resize_image(image)
+image = grayscale(image)
+
+ascii_data = pixels_to_ascii(image)
+
+# format into proper width
+width = image.width
+ascii_img = "\n".join(
+    [ascii_data[i:i+width] for i in range(0, len(ascii_data), width)]
+)
+
+print(ascii_img)
+
+
+
+with open("ascii_art.txt", "w") as f:
+    f.write(ascii_img)
+
+print("\n✅ ASCII art saved as ascii_art.txt")
