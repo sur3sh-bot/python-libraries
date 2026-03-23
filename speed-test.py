775ef334ad39f6ad31760cfad39e51c9958efb5e
K C Suresh Naidu <sur3sh.naidu@gmail.com>
2026-03-23 22:48:32 +0530

    Add comment to clarify the purpose of get_best_server method in speed-test.py

--- speed-test.py
+++ speed-test.py
@@ -3,7 +3,7 @@ import speedtest #pip install speedtest-cli
 print("\n Running internet speed test...\n")
 try:
     st = speedtest.Speedtest()
-    st.get_best_server()
+    st.get_best_server() #this method finds the best server based on ping
     print(f"Download: {st.download() / 1_000_000:.2f} Mbps")
     print(f"Upload:   {st.upload() / 1_000_000:.2f} Mbps")
     print(f"Ping:     {st.results.ping:.2f} ms")
