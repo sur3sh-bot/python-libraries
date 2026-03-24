3730f56e815020a16544876d2092144c237455ee
K C Suresh Naidu <sur3sh.naidu@gmail.com>
2026-03-24 22:35:05 +0530

    Add comment to explain GET request in check_url function

--- url-status.py
+++ url-status.py
@@ -2,7 +2,7 @@ import requests #pip install requests
 #this library is used to make HTTP requests in Python. It allows you to send HTTP requests and handle responses easily.
 def check_url(url):
     try:
-        response = requests.get(url, timeout=5)
+        response = requests.get(url, timeout=5) #sends a GET request to the specified URL with a timeout of 5 seconds. If the server does not respond within this time, it will raise an exception.
         return response.status_code
     except:
         return "Error"
